950a552ba448553061a2ffe24d34169c59cee0de
XDTAWANPRO <77896938+XDTAWANPRO@users.noreply.github.com>
2024-11-12 00:43:33 +0700

    Update TP.lua

--- TP.lua
+++ TP.lua
@@ -1,10 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/lime"))()
 
-local w = Library:Window("หนีตาย!!")
+local w = Library:Window("หนีตาย")
 
-w:Button("วาป(โลก2)", function()
-   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new()
-end)
-w:Button("วาป(โลก3)", function()
+w:Button("วาปหนี", function()
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new()
 end)
